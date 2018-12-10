1fbbd5e95c21a235ec0f48c70aec5c1bebfbeff1
jcasas5253 <skedabble@gmail.com>
2018-12-09 18:45:50 -0800

    added displayGifInfo function; created an Ajax call; dynamically created a div to hold the gifs

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,1 +1,21 @@
 var buttons = ["Dog", "Cat", "Rabbit", "Hamster", "Skunk", "Goldfish", "Bird", "Ferret", "Turtle", "Sugar Glider", "Chinchilla", "Hedgehog", "Hermit Crab", "Gerbil", "Pygmy Goat", "Chicken", "Capybara", "Teacup Pig", "Serval", "Salamander", "Frog",];
+
+// displayGifInfo function re-renders the HTML
+function displayGifInfo() {
+
+    var animals = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animals + "&api_key=PIem49TSSafPP8CLXggUeldwXQPZ6th4&limit=10";
+
+    // Creating an AJAX call for the gif button being clicked
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        displayGif(response);
+        console.log(response);
+
+        // Creating a div to hold the gifs
+        var giphyDiv = $("<div class='giphy'>");
+    });
+
+}
